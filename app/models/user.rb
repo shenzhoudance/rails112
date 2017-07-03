0d60e5d7ce58969b19edb957ec0772a3aaae094f
xiaowei <shenzhoudance@gmail.com>
2017-07-03 10:47:17 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@ has_many :posts
         participated_groups.include?(group)
       end
 
+      def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
 end
