899a2c5122cf68c0312b978ce4ddbdde0cc318b0
xiaowei <shenzhoudance@gmail.com>
2017-07-03 09:35:25 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,9 +2,10 @@ class GroupsController < ApplicationController
 before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
-   def show
-     @group = Group.find(params[:id])
-   end
+def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts
+  end
 
    def edit
       find_group_and_check_permission
